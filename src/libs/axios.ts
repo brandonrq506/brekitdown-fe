73355023bcf2542b93076eebfd17a9e2eb321e37
Brandon Ramirez <darkrahd@gmail.com>
2026-08-11 17:13:03 -0600

    [7] Creating base instance and env variables

---
+++ src/libs/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const TOKEN = import.meta.env.VITE_DEV_TOKEN;
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+export const GOALS_ENDPOINT = "/goals";
