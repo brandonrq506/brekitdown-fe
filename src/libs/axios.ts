733f7cd71df57149303a70269b1e791fb7ebafb7
Brandon Ramirez <darkrahd@gmail.com>
2026-08-11 17:45:43 -0600

    [7] Improving Axios setup

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -3,8 +3,16 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 const TOKEN = import.meta.env.VITE_DEV_TOKEN;
 
+if (!API_URL) throw new Error("VITE_API_URL is not defined in the env variables.");
+
+if (!TOKEN) throw new Error("VITE_DEV_TOKEN is not defined in the env variables.");
+
 export const api = axios.create({
   baseURL: API_URL,
+  timeout: 5_000,
+  transitional: {
+    clarifyTimeoutError: true,
+  },
   headers: {
     Authorization: `Bearer ${TOKEN}`,
   },
