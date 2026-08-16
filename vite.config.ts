347c455bd42d0a26fd33762bade611eec7302ee5
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2026-08-15 21:02:14 -0600

    [10] Installing and setup Tanstack Router (#18)
    * [10] Installing libraries
    
    * [10] Configuring router pluging
    
    * [10] Ignore routeTree managed by Tanstack router
    
    * [11] Fixing "Fast Refresh" false positive linting warning
    
    * [10] Adding main and routeTree

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, lazyPlugins } from "vite-plus";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +12,13 @@ export default defineConfig({
     "*": "vp check --fix",
   },
   fmt: {
+    ignorePatterns: ["src/routeTree.gen.ts"],
     sortTailwindcss: {
       stylesheet: "./src/index.css",
     },
   },
   lint: {
+    ignorePatterns: ["src/routeTree.gen.ts"],
     plugins: ["react", "typescript", "oxc"],
     rules: {
       "react/rules-of-hooks": "error",
@@ -27,6 +30,14 @@ export default defineConfig({
       ],
       "vite-plus/prefer-vite-plus-imports": "error",
     },
+    overrides: [
+      {
+        files: ["src/routes/**/*.tsx"],
+        rules: {
+          "react/only-export-components": "off",
+        },
+      },
+    ],
     options: {
       typeAware: true,
       typeCheck: true,
@@ -38,5 +49,14 @@ export default defineConfig({
       },
     ],
   },
-  plugins: lazyPlugins(() => [react(), tailwindcss()]),
+  plugins: lazyPlugins(() => [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      quoteStyle: "double",
+      semicolons: true,
+    }),
+    tailwindcss(),
+    react(),
+  ]),
 });
