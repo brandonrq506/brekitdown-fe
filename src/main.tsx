a03785985a1fac5fa0e344c6ebcd9b731e5d9a09
Brandon Ramirez <darkrahd@gmail.com>
2026-08-15 20:56:10 -0600

    [10] Adding main and routeTree

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")!).render(
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
