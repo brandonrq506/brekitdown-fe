8286874cb91511192bd03658d214e9aa1639c510
Brandon Ramirez <darkrahd@gmail.com>
2026-08-16 18:49:49 -0600

    [29] Removing duplicate axios functions

--- src/features/goals/api/getGoals.ts
+++
@@ -1,8 +0,0 @@
-import { api, GOALS_ENDPOINT } from "@/libs/axios";
-import type { QueryFunctionContext } from "@tanstack/react-query";
-import type { GoalsResponse } from "@/features/goals/types/goal";
-
-export const getGoals = async ({ signal }: QueryFunctionContext): Promise<GoalsResponse> => {
-  const { data } = await api.get<GoalsResponse>(GOALS_ENDPOINT, { signal });
-  return data;
-};
