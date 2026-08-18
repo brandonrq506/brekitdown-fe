5a1eb3da4149328d99791500a6afadb9456bc086
Brandon Ramirez <darkrahd@gmail.com>
2026-08-17 21:01:40 -0600

    [ad-hoc] Fixing "__" instead of "_".

--- src/routes/_protected/index.tsx
+++ src/routes/_protected/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import App from "@/App";
 
-export const Route = createFileRoute("/__protected/")({
+export const Route = createFileRoute("/_protected/")({
   component: RouteComponent,
 });
 
--- src/routes/_protected/route.tsx
+++ src/routes/_protected/route.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/__protected")({
+export const Route = createFileRoute("/_protected")({
   component: RouteComponent,
 });
 
