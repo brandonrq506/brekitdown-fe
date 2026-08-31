f72814079f94eae77e867602c995cdc9d500b8aa
Brandon Ramirez <darkrahd@gmail.com>
2026-08-31 13:10:14 -0600

    [83] Refactored `getGoal` to leverage more Tansack Query built-in helpers and types

--- src/features/goals/api/axios/getGoal.ts
+++ src/features/goals/api/axios/getGoal.ts
@@ -1,12 +1,17 @@
 import { api, GOALS_ENDPOINT } from "@/libs/axios";
+
 import type { QueryFunctionContext } from "@tanstack/react-query";
+import type { goalKeys } from "@/features/goals/api/queries";
 import type { GoalResponse } from "@/features/goals/types/goal";
 
-export const getGoal = async (
-  referenceXid: string,
-  { signal }: QueryFunctionContext,
-): Promise<GoalResponse> => {
-  const { data } = await api.get<GoalResponse>(`${GOALS_ENDPOINT}/${referenceXid}`, { signal });
+type GoalDetailQueryKey = ReturnType<typeof goalKeys.detail>;
 
-  return data;
+export const getGoal = async ({
+  signal,
+  queryKey: [{ referenceXid }],
+}: QueryFunctionContext<GoalDetailQueryKey>): Promise<GoalResponse> => {
+  const URL = `${GOALS_ENDPOINT}/${referenceXid}`;
+  const response = await api.get<GoalResponse>(URL, { signal });
+
+  return response.data;
 };
