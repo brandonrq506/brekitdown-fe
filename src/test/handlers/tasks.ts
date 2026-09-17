0cdbc7a51f8dcdceae2866b424c67ebbd16c37cd
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2026-09-16 22:08:08 -0600

    [141] Adding axios, tanstack query and handlers (#90)

--- src/test/handlers/tasks.ts
+++ src/test/handlers/tasks.ts
@@ -1,13 +1,17 @@
-import { HttpResponse, http } from "msw";
+import { HttpResponse, http, type HttpResponseInit } from "msw";
 
 import { apiRoutes } from "@/test/handlers/api-routes";
-import { tasks } from "@/test/store/tasks";
+import { newlyCreatedTask, tasks } from "@/test/store/tasks";
 
-import type { Task, TasksResponse } from "@/features/tasks/types/task";
+import type { Task, TaskResponse, TasksResponse } from "@/features/tasks/types/task";
+
+export const mockTaskResponse = (data: Task, init?: HttpResponseInit) =>
+  HttpResponse.json<TaskResponse>({ data }, init);
 
 export const mockTasksResponse = (data: Task[]) => HttpResponse.json<TasksResponse>({ data });
 
 export const taskHandlers = [
   http.get(apiRoutes.tasks, () => mockTasksResponse(tasks)),
+  http.post(apiRoutes.tasks, () => mockTaskResponse(newlyCreatedTask, { status: 201 })),
   http.delete(apiRoutes.task(), () => new HttpResponse(null, { status: 204 })),
 ];
